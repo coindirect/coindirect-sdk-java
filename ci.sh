bf58ce2987cc593c7b71f01fdf4989f53499cbfd
Husayn Arrah <husayn.arrah@gmail.com>
2019-08-18 19:29:53 +0200

    deploy

--- ci.sh
+++ ci.sh
@@ -6,7 +6,6 @@ eval $(gpg-agent --daemon --no-grab --write-env-file $HOME/.gpg-agent-info)
 export GPG_TTY=$(tty)
 export GPG_AGENT_INFO
 
-# apt-get update && apt-get install -qy xmlstarlet libxml2-utils
 echo -e "$GPG_KEY_SECRET" | gpg --batch --import
 gpg --list-keys
-mvn clean install -Prelease -s settings.xml
+mvn clean deploy -Prelease -s settings.xml
