65a4b214be5489872b24466cd52e9bfe4d59d3cc
Husayn Arrah <husayn.arrah@gmail.com>
2019-08-18 18:08:16 +0200

    use gpg agent

--- ci.sh
+++ ci.sh
@@ -2,7 +2,9 @@
 
 set -e
 
+eval $(gpg-agent --daemon --no-grab --write-env-file $HOME/.gpg-agent-info)
 export GPG_TTY=$(tty)
+export GPG_AGENT_INFO
 
 apt-get update && apt-get install -qy xmlstarlet libxml2-utils
 echo -e "$GPG_KEY_SECRET" | gpg --import
